40ea1864f42bb923f688e625e5ffe5d71ad26467
William Smith <williampsmith@berkeley.edu>
2016-11-06 20:39:11 -0800

    typos and bugs

--- powersets.py
+++ powersets.py
@@ -1,31 +1,32 @@
 # assumes s is an ordered set (list, in python)
 # time: O(n * 2^n) where n is the length of the set
 def powersets(s):
-	P = []
+	powerset = []
 	N = 1 << len(s) # save length of the powerset
 	for i in range(N): # iterate through all permutations 
 		subset = [s[bit] for bit in range(len(s)) if (i & (1 << bit))]
-		P.append(subset)
-	return P
+		powerset.append(subset)
+	return powerset
 
 # time: O(s) where s is the number of set bits 
 def number_of_ones(a):
-	count = 0;
+	count = 0
 	while a != 0:
-		count += 1; 
+		count += 1
 		a = a & (a - 1) # remove least significant flipped bit
 	return count
 
+# returns set of length-k subsets of s
 # time: O(n * 2^n) where n is the length of the set
 def k_sets(s, k):
-	P = []
+	powerset = []
 	N = 1 << len(s) 
 	for i in range(N): 
 		if number_of_ones(i) != k: # check if the i'th permutation includes k elements 
 			continue
 		subset = [s[bit] for bit in range(len(s)) if (i & (1 << bit))]
-		P.append(subset)
-	return P
+		powerset.append(subset)
+	return powerset
 
 # NOT CORRECT CURRENTLY
 def powersets_2(s):
