438581c39b69cb6d4acb2094858983af86238afb
William Smith <williampsmith@berkeley.edu>
2017-08-20 19:06:22 -0700

    simplify code

--- most_water.py
+++ most_water.py
@@ -2,28 +2,15 @@ def maxArea(heights):
         """
         :type heights: List[int]
         Given a list of wall heights, where heights[i] = j indicates that there
-        is a wall of height j at x = i, return the max inner area created by two
+        is a wall of heights j at x = i, return the max inner area created by two
         walls within heights, i.e. the pair of walls that would hold the most water.
         """
-        def calc_area(point1, point2):
-            len = point2[0] - point1[0]
-            return len * min(point1[1], point2[1])
-        def reset_indices(i, j, heights):
-            if heights[i] < heights[j]:
-                return i + 1, j
-            return i, j - 1
-
-        heights_len = len(heights)
-        if heights_len < 2:
-            return 0
-
-        points = zip(range(heights_len), heights)
-        max_area = calc_area(points[0], points[-1])
-        i, j = reset_indices(0, len(heights) - 1, heights)
-
+        max_area = 0
+        i, j = 0, len(heights) - 1
         while i < j:
-            temp_area = calc_area(points[i], points[j])
-            if temp_area > max_area:
-                max_area = temp_area
-            i, j = reset_indices(i, j, heights)
+            max_area = max(max_area, min(heights[i], heights[j]) * (j - i))
+            if heights[i] < heights[j]:
+                i += 1
+            else:
+                j -= 1
         return max_area
